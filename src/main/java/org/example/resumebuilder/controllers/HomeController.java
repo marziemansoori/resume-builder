8ab398dc8a18d7e9b32677fb821b9f497a5453c4
marziemansoori <marzie.masnoori@gmail.com>
2024-03-25 22:28:54 +0000

    Add CV template

--- src/main/java/org/example/resumebuilder/controllers/HomeController.java
+++ src/main/java/org/example/resumebuilder/controllers/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
@@ -24,6 +23,6 @@ public class HomeController {
     @GetMapping("/profile/{userId}")
     public String profile(@PathVariable String userId, Model model) {
         model.addAttribute("userId", userId);
-        return "Profile";
+        return "profile";
     }
 }
