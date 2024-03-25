583c3434a570204655989c57b6d247fbf648db28
marziemansoori <marzie.masnoori@gmail.com>
2024-03-25 20:57:31 +0000

    Add profile view path + html for profile

--- src/main/java/org/example/resumebuilder/controllers/HomeController.java
+++ src/main/java/org/example/resumebuilder/controllers/HomeController.java
@@ -1,14 +1,29 @@
 package org.example.resumebuilder.controllers;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
         return "Home Page";
+    }
+
+
+    @GetMapping("/edit")
+    public String edit() {
+        return "Edit page";
+    }
+
+    @GetMapping("/profile/{userId}")
+    public String profile(@PathVariable String userId, Model model) {
+        model.addAttribute("userId", userId);
+        return "Profile";
     }
 }
